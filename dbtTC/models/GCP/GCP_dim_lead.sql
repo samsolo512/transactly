47ba61fe711ea2772dca2ae2f4910d2b9ddba0dc
sam-brown-512 <sam.brown@transactly.com>
2022-09-08 23:50:30 -0600

    removed owner fields from dim_lead

--- dbtTC/models/GCP/GCP_dim_lead.sql
+++ dbtTC/models/GCP/GCP_dim_lead.sql
@@ -28,19 +28,8 @@ with
             ,email
             ,lead_source
             ,created_date
-            ,owner_first_name
-            ,owner_last_name
             ,owner_name
-            ,owner_title
-            ,owner_street
-            ,owner_city
-            ,owner_postal_code
-            ,owner_country
-            ,owner_email
-            ,owner_phone
-            ,owner_mobile_phone
-            ,owner_is_active_flag
-        
+
         from dim_lead
     )
 
--- dbtTC/models/dim/dim_lead.sql
+++ dbtTC/models/dim/dim_lead.sql
@@ -32,18 +32,18 @@ with
             ,l.lead_source
             ,l.created_date
 
-            ,u.first_name as owner_first_name
-            ,u.last_name as owner_last_name
+--             ,u.first_name as owner_first_name
+--             ,u.last_name as owner_last_name
             ,u.name as owner_name
-            ,u.title as owner_title
-            ,u.street as owner_street
-            ,u.city as owner_city
-            ,u.postal_code as owner_postal_code
-            ,u.country as owner_country
-            ,u.email as owner_email
-            ,u.phone as owner_phone
-            ,u.mobile_phone as owner_mobile_phone
-            ,case when u.is_active = 'TRUE' then 1 else 0 end as owner_is_active_flag
+--             ,u.title as owner_title
+--             ,u.street as owner_street
+--             ,u.city as owner_city
+--             ,u.postal_code as owner_postal_code
+--             ,u.country as owner_country
+--             ,u.email as owner_email
+--             ,u.phone as owner_phone
+--             ,u.mobile_phone as owner_mobile_phone
+--             ,case when u.is_active = 'TRUE' then 1 else 0 end as owner_is_active_flag
 
         from
             src_sf_lead l
