7200fa0704a59f0cb3bae35cc6eddf36245c93b1
sam-brown-512 <sam.brown@transactly.com>
2022-09-08 23:51:18 -0600

    removed owner fields from dim_lead

--- Snowflake/create/7_create_table_GCP_dim_lead.sql
+++ Snowflake/create/7_create_table_GCP_dim_lead.sql
@@ -15,16 +15,5 @@ create or replace table data_warehouse.dim_lead (
     ,email string
     ,lead_source string
     ,created_date date
-    ,owner_first_name string
-    ,owner_last_name string
     ,owner_name string
-    ,owner_title string
-    ,owner_street string
-    ,owner_city string
-    ,owner_postal_code string
-    ,owner_country string
-    ,owner_email string
-    ,owner_phone string
-    ,owner_mobile_phone string
-    ,owner_is_active_flag int
 )
