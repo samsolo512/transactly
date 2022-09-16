9866884352550142d90fab5e9f646ee09631f37b
sam-brown-512 <sam.brown@transactly.com>
2022-09-15 21:32:20 -0600

    small modifications to macro

--- dbtTC/macros/field_clean.sql
+++ dbtTC/macros/field_clean.sql
@@ -3,11 +3,13 @@
     (regexp_replace(
         regexp_replace(
             {{ field_name }},
-            '[\r\n]',  -- replace line returns with a space
+            '[\r\n]',  -- replace these items with a space
             ' '
         ),
-        '[\"]',  -- replace quotation marks with no space
+        '[\"\^]',  -- replace these items with no space
         ''
     ))
 
 {% endmacro %}
+
+-- a.address_line_1 regexp '.*[^a-zA-Z0-9 \.()#,\-\/\'{}:&\*_@`].*'
