3f588e65a567953d89991d0641aaa9d550a0d8fd
sam-brown-512 <sam.brown@transactly.com>
2022-09-26 23:38:14 -0600

    added custom queries to support Madison's requests

---
+++ dbtTC/models/Custom/DIY_w_TC_on_same_side.sql
@@ -0,0 +1,54 @@
+with
+    fact_transaction_member as(
+        select *
+        from {{ ref('fact_transaction_member') }}
+    )
+
+    ,dim_user as(
+        select *
+        from {{ ref('dim_user') }}
+    )
+
+    ,dim_member as(
+        select *
+        from {{ ref('dim_member') }}
+    )
+
+    ,dim_transaction as(
+        select *
+        from {{ ref('dim_transaction') }}
+    )
+
+    ,final as(
+        select
+            t.street
+            ,t.city
+            ,t.state
+            ,t.zip
+
+            ,m.first_name as member_first_name
+            ,m.last_name as member_last_name
+            ,m.email as member_email
+            ,m.role_name as member_role
+            ,m.office_name as member_office_name
+
+            ,u.first_name as assigned_tc_first_name
+            ,u.last_name as assigned_tc_last_name
+            ,u.email as assigned_tc_email
+
+            ,t.side_id as transaction_side
+            ,m.side_id as member_side
+
+        from
+            fact_transaction_member fact
+            join dim_user u on fact.user_pk = u.user_pk
+            join dim_member m on fact.member_pk = m.member_pk
+            join dim_transaction t on fact.transaction_pk = t.transaction_pk
+
+        where
+            m.role_name in('Seller Transaction Coordinator','Buyer Transaction Coordinator')
+            and t.diy_flag = 1
+            and t.side_id = m.side_id
+    )
+
+select * from final
---
+++ dbtTC/models/Custom/Transaction_created_user_different_than_transaction_agent.sql
@@ -0,0 +1,70 @@
+with
+    fact_order as(
+        select *
+        from {{ ref('fact_order') }}
+    )
+
+    ,dim_order as(
+        select *
+        from {{ ref('dim_order') }}
+    )
+
+    ,dim_user as(
+        select *
+        from {{ ref('dim_user') }}
+    )
+
+    ,dim_transaction as(
+        select *
+        from {{ ref('dim_transaction') }}
+    )
+
+    ,dim_date as(
+        select *
+        from {{ ref('dim_date') }}
+    )
+
+    ,final as(
+        select
+            t.street as transaction_street
+            ,t.city as transaction_city
+            ,t.state as transaction_state
+            ,t.zip as transaction_zip
+            ,u.first_name as user_first_name
+            ,u.last_name as user_last_name
+            ,u.email as user_email
+            ,o.office_name
+            ,ua.first_name as assigned_tc_first_name
+            ,ua.last_name as assigned_tc_last_name
+            ,ua.email as assigned_tc_email
+            ,uc.first_name as created_by_first_name
+            ,uc.last_name as created_by_last_name
+            ,uc.email as created_by_email
+        from
+            fact_order fact
+            join dim_order o on fact.order_pk = o.order_pk
+            join dim_transaction t on fact.transaction_pk = t.transaction_pk
+            join dim_user ua on fact.assigned_tc_pk = ua.user_pk
+            join dim_user uc on fact.created_by_pk = uc.user_pk
+            join dim_user u on fact.user_pk = u.user_pk
+            left join dim_date closed_date on fact.closed_date_pk = closed_date.date_pk
+        where
+            u.user_pk <> uc.user_pk
+            and(
+                nvl(o.office_name, 'a') not in(
+                    'TC Solutions Clients'
+                    ,'The NOMA Group'
+                    ,'NOMA Group Corporate'
+                    ,'Noma-Venture REI'
+                    ,'The Noma Group - NV'
+                    ,'Renters Warehouse'
+                    ,'Renters Warehouse - GA'
+                    ,'Renters Warehouse - AZ'
+                    ,'Renters Warehouse - NV'
+                    ,'Renters Warehouse - FL'
+                    ,'Renters Warehouse - NC'
+                )
+            )
+    )
+
+select * from final
