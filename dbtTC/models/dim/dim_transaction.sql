ff16bc5d56f3aaba4646b648d84056de664fb474
sam-brown-512 <sam.brown@transactly.com>
2022-10-04 09:31:44 -0600

    added user and created by first and last names to dim_transaction

--- dbtTC/models/dim/dim_transaction.sql
+++ dbtTC/models/dim/dim_transaction.sql
@@ -24,6 +24,11 @@ with
         from {{ ref('src_tc_transaction_status') }}
     )
 
+    ,src_tc_user as(
+        select *
+        from {{ ref('src_tc_user') }}
+    )
+
     ,diy as(
         select
             t.transaction_id
@@ -55,6 +60,8 @@ with
             working.seq_dim_transaction.nextval as transaction_pk
             ,t.transaction_id
             ,t.user_id
+            ,u.first_name as user_first_name
+            ,u.last_name as user_last_name
             ,ts.status
             ,t.type_id
             ,p.party_name as side_id
@@ -62,6 +69,8 @@ with
             ,t.created_date
             ,t.closed_date
             ,t.created_by_id
+            ,cbu.first_name as created_by_first_name
+            ,cbu.last_name as created_by_last_name
             ,a.street
             ,a.city
             ,a.state
@@ -76,6 +85,8 @@ with
                 and a.state = nd.state
                 and t.status_changed_date = nd.status_changed_date
             join src_tc_transaction_status ts on t.status_id = ts.transaction_status_id
+            join src_tc_user u on t.user_id = u.user_id
+            join src_tc_user cbu on t.created_by_id = cbu.user_id
             left join src_tc_party p on t.side_id = p.party_id
             left join diy
                 on t.transaction_id = diy.transaction_id
