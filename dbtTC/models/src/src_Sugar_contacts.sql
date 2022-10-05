d89d89183f9697e26b218fdb4fc105de6a84750c
sam-brown-512 <sam.brown@transactly.com>
2022-10-05 14:47:21 -0600

    added shortcut script in comments that can use to create cols for wide tables

--- dbtTC/models/src/src_Sugar_contacts.sql
+++ dbtTC/models/src/src_Sugar_contacts.sql
@@ -1,3 +1,31 @@
+-- shortcut to create columns for this table
+
+/*
+ select
+    column_name
+    ,data_type
+    ,concat(
+        ',{{ field_clean(\047"'
+        ,column_name
+        ,'"\047) }} as '
+        ,column_name
+        -- ,case
+        --     when data_type = 'TEXT' then 'string'
+        --     when data_type like 'TIME%' then 'datetime'
+        --     when data_type = 'DATE' then 'date'
+        --     when data_type like 'BOOL%' then 'int'
+        --     when data_type like 'NUM%' then 'numeric'
+        -- else null
+        -- end
+    )
+from skyvia.INFORMATION_SCHEMA.COLUMNS
+where
+    lower(table_name) = 'contacts'
+order by ordinal_position
+;
+ */
+
+
 with
     src_Sugar_contacts as(
         select *
