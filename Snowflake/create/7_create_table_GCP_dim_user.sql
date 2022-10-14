1d734ecc22fa115796491c1bcc0896b1e4468b1d
sam-brown-512 <sam.brown@transactly.com>
2022-10-14 17:17:20 -0600

    updated user with new fields that include salesforce leads

--- Snowflake/create/7_create_table_GCP_dim_user.sql
+++ Snowflake/create/7_create_table_GCP_dim_user.sql
@@ -1,32 +1,61 @@
 -- dim_user
 
 create or replace table data_warehouse.dim_user (
-    user_id int
+    user_pk int
+    ,user_id int
+    ,lead_id string
     ,first_name string
     ,last_name string
     ,fullname string
     ,email string
     ,brokerage string
     ,subscription_level string
-    ,lead_status string
+    ,transaction_coordinator_status string
+    ,contact_owner string
+    ,contact_id string
 
-    -- flags
+    -- agent address
+    ,agent_name string
+    ,agent_email string
+    ,address string
+    ,address2 string
+    ,original_sales_rep_name string
+
+    -- lead address
+    ,lead_street string
+    ,lead_city string
+    ,lead_state string
+    ,lead_zip string
+    ,lead_country string
+    ,full_address string
+
+    --flags
     ,pays_at_title_flag int
     ,eligible_for_clients_flag int
     ,tc_staff_flag int
     ,tc_client_flag int
---     ,diy_flag int
+    ,lead_flag int
     ,self_procured_flag int
 
+    --HubSpot fields
+    ,HS_agent_type string
+    ,transactly_home_insurance_vendor_status string
+    ,transactly_utility_connection_vendor_status string
+
     -- dates
+    ,user_created_date date
+    ,lead_created_date date
     ,start_date date
     ,days_between_start_date_and_first_order_date int
-    ,last_order_placed date
-    ,last_order_due datetime
-    ,tier_3 date
+    ,tier_3 datetime
     ,tier_2 datetime
     ,tier_1 datetime
+    ,last_order_due datetime
     ,first_order_placed date
+    ,last_order_placed date
     ,first_order_closed date
+    ,second_order_closed date
+    ,third_order_closed date
+    ,fourth_order_closed date
     ,fifth_order_closed date
 )
--- dbtTC/models/GCP/GCP_dim_user.sql
+++ dbtTC/models/GCP/GCP_dim_user.sql
@@ -13,33 +13,62 @@ with
 
     ,final as(
         select
-            user_id
+            user_pk
+            ,user_id
+            ,lead_id
             ,first_name
             ,last_name
             ,fullname
             ,email
             ,brokerage
             ,subscription_level
-            ,transaction_coordinator_status as lead_status
-
-            -- flag
+            ,transaction_coordinator_status
+            ,contact_owner
+            ,contact_id
+        
+            -- agent address
+            ,agent_name
+            ,agent_email
+            ,address
+            ,address2
+            ,original_sales_rep_name
+        
+            -- lead address
+            ,lead_street
+            ,lead_city
+            ,lead_state
+            ,lead_zip
+            ,lead_country
+            ,full_address
+        
+            --flags
             ,pays_at_title_flag
             ,eligible_for_clients_flag
             ,tc_staff_flag
             ,tc_client_flag
---             ,diy_flag
+            ,lead_flag
             ,self_procured_flag
-
-            -- dates
+        
+            --HubSpot fields
+            ,HS_agent_type
+            ,transactly_home_insurance_vendor_status
+            ,transactly_utility_connection_vendor_status
+        
+            --s
+            ,user_created_date
+            ,lead_created_date
             ,start_date
             ,days_between_start_date_and_first_order_date
-            ,last_order_placed
-            ,last_order_due
             ,tier_3
             ,tier_2
             ,tier_1
+            ,last_order_due
             ,first_order_placed
+            ,last_order_placed
             ,first_order_closed
+            ,second_order_closed
+            ,third_order_closed
+            ,fourth_order_closed
             ,fifth_order_closed
         from dim_user
     )
