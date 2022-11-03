798740d0b9abc2f54bbc780f848ff24b378015c1
sam-brown-512 <sam.brown@transactly.com>
2022-11-03 09:30:44 -0600

    reorg fact_transaction_member_contact - moved case statements to 'with' clause

--- dbtTC/models/fact/fact_transaction_member_contact.sql
+++ dbtTC/models/fact/fact_transaction_member_contact.sql
@@ -70,76 +70,10 @@ with
                 and tv.vendor_type_id = 7  -- home insurance
     )
 
-    -- combine members and contacts
---     ,member_contact as(
---         -- member
---         select
---             trans.transaction_id
---             ,lower(memb.email) as email
---             ,m.member_id as member_contact_id
---             ,memb.first_name
---             ,memb.last_name
---             ,memb.role_name as role
---             ,1 as member_flag
---             ,0 as contact_flag
---
---         from
---             src_tc_transaction trans
---             join dim_transaction dt on trans.transaction_id = dt.transaction_id
---             join src_tc_member m on trans.transaction_id = m.transaction_id
---             left join dim_member memb on m.member_id = memb.member_id
---
---         -- contact
---         union all
---         select
---             trans.transaction_id
---             ,lower(cont.email) as email
---             ,cont.contact_id as member_contact_id
---             ,cont.first_name
---             ,cont.last_name
---             ,c.role_name as role
---             ,0 as member_flag
---             ,1 as contact_flag
---
---         from
---             src_tc_transaction trans
---             join dim_transaction dt on trans.transaction_id = dt.transaction_id
---             join src_tc_contact cont on trans.transaction_id = cont.transaction_id
---             left join dim_contact c on cont.contact_id = c.contact_id
---     )
-
-    -- all members and contacts
---     ,combine as(
---         select
---             transaction_id
---             ,lower(email) as email
---             ,max(member_id) as member_id
---             ,max(contact_id) as contact_id
---             ,first_name
---             ,last_name
---             ,member_role as member_role
---             ,contact_role as contact_role
---             ,max(member_flag) as member_flag
---             ,max(contact_flag) as contact_flag
---
---         from
---             member_contact mc
---
---         group by
---             transaction_id, lower(email), first_name, last_name, member_role, contact_role
---     )
-
-    ,final as(
+    -- combined util and insurance case statements
+    ,util_insurance as(
         select
-            t.transaction_pk
-            ,mc.member_contact_pk
-
-            -- utility and insurance info
-            ,ttv_utility.internal_name as utility_lead_sent_to
-            ,DATE(ttv_utility.notified_date) as utility_notified_date
-            ,ttv_home_insurance.internal_name as home_insurance_lead_sent_to
-            ,DATE(ttv_home_insurance.notified_date) as home_insurance_notified_date
-
+            t.transaction_id
             ,CASE
                 WHEN (ttv_utility.internal_name is not null) THEN 'LEAD SENT'
 
@@ -245,17 +179,43 @@ with
                 END as home_insurance_status
 
         from
+            dim_transaction t
+            join ttv_utility on ttv_utility.transaction_id = t.transaction_id
+            join ttv_home_insurance on ttv_home_insurance.transaction_id = t.transaction_id
+    )
+
+    ,final as(
+        select
+            t.transaction_pk
+            ,mc.member_contact_pk
+
+            -- utility and insurance info
+            ,u.internal_name as utility_lead_sent_to
+            ,date(u.notified_date) as utility_notified_date
+            ,i.internal_name as home_insurance_lead_sent_to
+            ,date(i.notified_date) as home_insurance_notified_date
+            ,ui.utility_transfer_status
+            ,ui.home_insurance_status
+
+            -- for testing in the 'where' clause below
+            -- ,mc.role_name as member_contact_role
+            -- ,t.status as transfer_status
+            -- ,t.transaction_side
+            -- ,t.contract_closing_date as closing_date
+
+        from
             src_tc_transaction trans
             join dim_transaction t on trans.transaction_id = t.transaction_id
             left join dim_member_contact mc on t.transaction_id = mc.transaction_id
-            left join ttv_utility on ttv_utility.transaction_id = t.transaction_id
-            left join ttv_home_insurance on ttv_home_insurance.transaction_id = t.transaction_id
+            left join ttv_utility u on u.transaction_id = t.transaction_id
+            left join ttv_home_insurance i on i.transaction_id = t.transaction_id
+            left join util_insurance ui on trans.transaction_id = ui.transaction_id
     )
 
 select * from final
 -- the following is for Alissa's report
 -- where
---     (contact_role = 'Buyer' or member_role = 'Buyer')
---     and status not in('cancelled', 'withdrawn')
---     and order_side = 'buyer'
+--     member_contact_role = 'Buyer'
+--     and transfer_status not in('cancelled', 'withdrawn')
+--     and transaction_side = 'buyer'
 --     and closing_date is not null
