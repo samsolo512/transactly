39cfc100a2943e7297fdaf517ac7b50ed3e506c6
sam-brown-512 <sam.brown@transactly.com>
2023-03-08 20:19:20 -0700

    purge bad chars in dim_user.email

--- dbtTC/macros/field_clean.sql
+++ dbtTC/macros/field_clean.sql
@@ -1,15 +1,20 @@
 {% macro field_clean(field_name) %}
 
-    (regexp_replace(
+    (
         regexp_replace(
-            {{ field_name }},
-            '[\r\n]',  -- replace these items with a space
-            ' '
-        ),
-        '[\"\^]',  -- replace these items with no space
-        ''
-    ))
+            regexp_replace(
+                regexp_replace(
+                    {{ field_name }},
+                    '[\r\n]',  -- replace these items with a space
+                    ' '
+                ),
+                '[\"\^]',  -- replace these items with no space
+                ''
+            ),
+            '[^a-zA-Z0-9\@\.\_\+\>\,\-\;\'\=\`]'  -- get rid of everything that arent these characters
+        )
+    )
 
 {% endmacro %}
 
--- a.address_line_1 regexp '.*[^a-zA-Z0-9 \.()#,\-\/\'{}:&\*\_\^\"@`].*'
+
